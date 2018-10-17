99a25ff8623b9603ffff8cbd675c2ffb6fcc828c
Igor <igormjq@gmail.com>
2018-10-17 16:09:28 -0300

    Added hashing method to newly created users and app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 app.use('/', routes);
 
 export default app;
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,20 @@
 'use strict';
+const { hashSync, genSaltSync } = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {});
+  }, {
+    onDelete: 'CASCADE',
+    hooks: {
+      beforeCreate(user) {
+        const salt = genSaltSync(10);
+        user.password = hashSync(user.password, salt);
+      },
+    }
+  });
   User.associate = function({ Match, Tournament }) {
     // this.hasMany(Match, { as: 'matches' });
     this.belongsToMany(Tournament, { foreignKey: 'userId', through: 'User_Tournament', as: 'tournaments', onDelete: 'CASCADE' });
