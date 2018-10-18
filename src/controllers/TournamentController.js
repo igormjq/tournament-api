f9284483ddc73204456d1016a96635523870b9e4
Igor <igormjq@gmail.com>
2018-10-18 17:12:59 -0300

    route for fetching all tournaments and a specific tournament by id added

---
+++ src/controllers/TournamentController.js
@@ -0,0 +1,33 @@
+import { Tournament } from '../models/'
+
+class TournamentController {
+
+  async getAll(req, res) {
+    
+    try {
+      let tournaments = await Tournament.findAll();
+      res.send({ tournaments });
+    } catch (error) {
+      res.status(400).send({ error });
+    }
+  }
+
+  async getById(req, res) {
+    let { id } = req.params;
+
+    try {
+      let tournament = await Tournament.findById(id);
+      
+      if(!tournament) {
+        throw "Tournament not found"
+      }
+
+      res.send({ tournament });
+
+    } catch (error) {
+      res.status(404).send({ error });
+    }
+  }
+}
+
+export default TournamentController;
